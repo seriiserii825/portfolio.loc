588d4b0dd45a601232896c3dbe77d698df683067
serii <seriiburduja@gmail.com>
2018-01-23 01:18:19 +0200

    finish sidebar

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,21 +1,27 @@
-$(function () {
-  /*
-      https://www.jqueryscript.net/animation/Smooth-Mouse-Wheel-Scrolling-Plugin-With-jQuery-easeScroll.html
-      ===========================*/
-  $("html").easeScroll({
-    frameRate: 60,
-    animationTime: 1000,
-    stepSize: 90,
-    pulseAlgorithm: 1,
-    pulseScale: 8,
-    pulseNormalize: 1,
-    accelerationDelta: 20,
-    accelerationMax: 1,
-    keyboardSupport: true,
-    arrowScroll: 50,
-    touchpadSupport: true,
-    fixedBackground: true
-  });
+$(function(){
+    /*
+        https://www.jqueryscript.net/animation/Smooth-Mouse-Wheel-Scrolling-Plugin-With-jQuery-easeScroll.html
+        ===========================*/
+    $("html").easeScroll({
+        frameRate: 60,
+        animationTime: 1000,
+        stepSize: 90,
+        pulseAlgorithm: 1,
+        pulseScale: 8,
+        pulseNormalize: 1,
+        accelerationDelta: 20,
+        accelerationMax: 1,
+        keyboardSupport: true,
+        arrowScroll: 50,
+        touchpadSupport: true,
+        fixedBackground: true
+    });
 
-
+    /* slider
+    ====================================================*/
+    $('#slider').slick({
+        appendArrows: $('.buttons-wrap'),
+        prevArrow: '<button class="btn btn-left" id="js-btn-left"><i class="icon icon-btn-bg"></i><span>prev</span></button>',
+        nextArrow: '<button class="btn btn-right" id="js-btn-right"><span class="icon icon-btn--right-bg"></span><span>next</span></button>'
+    });
 });
